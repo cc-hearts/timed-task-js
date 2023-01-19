05c4ddc387cfb70087201c3306fcd782aeaa4a0d
heart <7362469@qq.com>
2023-01-20 00:06:44 +0800

    chore: update puppeteer dockerfile

--- deploy-puppeteer.sh
+++ deploy-puppeteer.sh
@@ -3,6 +3,6 @@
 docker rm -f puppeterr-interval
 docker rmi -f puppeterr-interval:1.0.0
 
-docker build -t puppeterr-interval:1.0.0 .
+docker build -f ./packages/puppeterr/Dockerfile -t puppeterr-interval:1.0.0 .
 
 docker run --name puppeterr-interval -d puppeterr-interval:1.0.0
