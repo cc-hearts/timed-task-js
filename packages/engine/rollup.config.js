5a16fbae060a56c3a2a049b58f46bc9bd82fc599
heart <7362469@qq.com>
2023-01-19 20:22:55 +0800

    chore: add rollup config

--- packages/engine/rollup.config.js
+++ packages/engine/rollup.config.js
@@ -3,10 +3,15 @@ import json from "@rollup/plugin-json";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
+import cpConfigFile from "../../scripts/cp-config-file.js";
 import { fileURLToPath } from "url";
 const resolveFile = function (filePath) {
   return join(fileURLToPath(import.meta.url), "..", filePath);
 };
+
+const configPath = resolveFile("../config/config/app.yaml");
+
+const targetPath = resolveFile("./dist/app.yaml");
 
 export default {
   input: resolveFile("./index.ts"),
@@ -14,12 +19,13 @@ export default {
     file: resolveFile("./dist/index.js"),
     format: "esm",
   },
-  external: ['@prisma/client'],
+  external: ["@prisma/client"],
   plugins: [
     resolve(),
     json(),
     commonjs(),
     typescript(),
+    cpConfigFile(configPath, targetPath),
     // babel({
     //   presets: ["@babel/preset-env"],
     // }),
---
+++ scripts/cp-config-file.js
@@ -0,0 +1,11 @@
+import { copyFile } from "fs/promises";
+export default function cpConfigFile(copiedPath, resultPath) {
+  return {
+    name: "cp-config-file",
+    buildEnd() {
+      setTimeout(() => {
+        copyFile(copiedPath, resultPath);
+      }, 300);
+    },
+  };
+}
